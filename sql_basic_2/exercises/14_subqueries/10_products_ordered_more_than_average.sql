1ac33b1555a2ce54fb156ab2f9b66ae4d811a47b
haerde91 <harmidamateusz91@gmail.com>
2026-06-29 12:56:54 +0200

    SQL: add subquery exercises

--- sql_basic_2/exercises/14_subqueries/10_products_ordered_more_than_average.sql
+++ sql_basic_2/exercises/14_subqueries/10_products_ordered_more_than_average.sql
@@ -1,1 +1,13 @@
 --Task: Find products that were ordered more times than the average number of orders per product.
+select p.product_name
+from products p
+join order_items oi
+on p.product_id = oi.product_id
+group by p.product_name, p.product_id
+having count(oi.product_id) >
+    (
+    select avg(total_number_of_orders) 
+    from (select product_id, count(product_id) as total_number_of_orders
+    from order_items
+    group by product_id) x
+    );
