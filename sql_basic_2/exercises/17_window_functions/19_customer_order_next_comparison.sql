95c1abbc7f33c0001851e7813a8196290066931e
haerde91 <harmidamateusz91@gmail.com>
2026-08-15 01:54:16 +0200

    SQL: add window functions  exercise

---
+++ sql_basic_2/exercises/17_window_functions/19_customer_order_next_comparison.sql
@@ -0,0 +1,43 @@
+-- Task:
+-- The company wants to compare every order
+-- with the next order placed by the same customer.
+-- Return:
+-- order_id,
+-- customer_id,
+-- order_date,
+-- order_value,
+-- next_order_value,
+-- difference_to_next_order
+with orders_values as
+(
+    select oi.order_id,
+    sum(oi.quantity*p.price) as order_value
+    from order_items oi
+    join products p
+    on oi.product_id = p.product_id
+    group by oi.order_id
+),
+customers_and_orders_metrics as
+(
+    select ov.order_id,
+    o.customer_id,
+    o.order_date,
+    ov.order_value,
+    lead(ov.order_value) over (
+        partition by o.customer_id
+        order by o.order_date,
+        ov.order_id
+    ) as next_order_value
+    from orders_values ov
+    join orders o
+    on ov.order_id = o.order_id 
+
+)
+select caom.order_id,
+caom.customer_id,
+caom.order_date,
+caom.order_value,
+caom.next_order_value,
+(caom.next_order_value - caom.order_value) as difference_to_next_order
+from customers_and_orders_metrics caom
+;
