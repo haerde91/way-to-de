ed092bdc767cb420f1242ef375e15600684c7891
haerde91 <harmidamateusz91@gmail.com>
2026-08-22 20:48:00 +0200

    python - first meet

---
+++ Python_basic_1/01_variables.py
@@ -0,0 +1,27 @@
+# ============================================
+# 1. VARIABLES
+# ============================================
+
+# A variable stores a value that we can use later.
+
+name = "Mateusz"
+age = 29
+city = "Warsaw"
+
+print(name)
+print(age)
+print(city)
+
+print(type(name))
+print(type(age))
+print(type(city))
+
+# Change the value of the variable
+age = 30
+
+print(age)
+
+########################
+next_year_age = age + 1
+
+print(next_year_age)
---
+++ Python_basic_1/pythonbasic.md
@@ -0,0 +1,18 @@
+01  Python & environment
+02  Variables
+03  Data types
+04  Operators
+05  if / elif / else
+06  Lists
+07  Tuples
+08  Dictionaries
+09  Sets
+10  for
+11  while
+12  Functions
+13  Scope
+14  Strings
+15  Comprehensions
+16  Exceptions
+17  Modules / imports
+18  Files
